5351f2caf0f9e76a11d800401a1826a91be4ad82
Vantol Bennett <71115937+titan-65@users.noreply.github.com>
2026-04-09 18:45:14 -0500

    feat: add feet module exports

---
+++ src/feet/index.ts
@@ -0,0 +1,3 @@
+export { detectScripts, type DetectedScript } from "./script-detector.ts";
+export { ProcessManager, type ManagedProcess, type StartOptions, type StopResult } from "./process-manager.ts";
+export { SessionManager, type TerminalSession, type CreateOptions } from "./session-manager.ts";
